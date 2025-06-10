5146df7fb89361093cc69a27e724e843a6f04262
punkushal <spycymagar@gmail.com>
2025-06-10 16:51:53 +0545

    job posting model class created

--- job_board/models.py
+++ job_board/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class job_posting(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    company = models.CharField(max_length=100)
+    salary = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
