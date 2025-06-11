2723fa675158f15abb889b53c7288e7a568bb135
punkushal <spycymagar@gmail.com>
2025-06-11 13:53:01 +0545

    render actual html file with active job postings

--- job_board/views.py
+++ job_board/views.py
@@ -1,6 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import job_posting
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Job Board</h1>')
+    active_postings = job_posting.objects.filter(is_active = True)
+    context = {
+        'job_postings': active_postings
+    }
+    return render(request, 'job_board/index.html', context)
